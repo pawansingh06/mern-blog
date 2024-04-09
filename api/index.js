6073619edccb3ab359c71bb34d2acf0f4661a7b2
Pawan Singh <pawan39m@gmail.com>
2024-04-10 01:17:27 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoos from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoos
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("monogoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
