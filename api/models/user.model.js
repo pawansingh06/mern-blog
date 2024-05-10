a15e0f78ea3b3f2bc00ebbee84c013e248583c9f
Pawan Singh <pawan39m@gmail.com>
2024-05-11 01:08:18 +0530

    add admin funcvtionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema(
       type:String,
       default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwaC2JFGV1ztlUeZeuElwPBFz2lJHjuxAn-w&usqp=CAU"
     },
+    isAdmin:{
+      type:Boolean,
+      default:false
+    },
   },
   { timestamps: true }
 );
